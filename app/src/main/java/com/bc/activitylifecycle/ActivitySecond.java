303bba4968e2315de8d85bdd3f3ec3efbd8b75e3
林宝川 <lbc@sample.com>
2016-08-04 14:46:51 +0800

    第一次作业完成：在日志里打印生命周期状态

--- app/src/main/java/com/bc/activitylifecycle/ActivitySecond.java
+++ app/src/main/java/com/bc/activitylifecycle/ActivitySecond.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,35 +16,60 @@ import android.widget.Toast;
  * Created by lsx on 2016/7/22.
  */
 public class ActivitySecond extends AppCompatActivity{
+    private static final String TAG = "ActivitySecond";
     private TextView mTextView;
     private Button mSendBackButton;
-    private EditText mNameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_activity);
-        String inforContent = getIntent().getStringExtra(MainActivity.BOOK_NAME_OF_CATEGORY_PHYSICAL);
-        //Toast.makeText(ActivitySecond.this, inforContent, Toast.LENGTH_SHORT).show();
-        mTextView = (TextView) findViewById(R.id.second_activity_text_view);
-
-        mTextView.setText(inforContent);
-
-        mNameEditText = (EditText) findViewById(R.id.second_activity_edit_view);
+        Log.d(TAG, "林宝川测试生命周期onCreate: 成功执行了");
         mSendBackButton = (Button) findViewById(R.id.second_activity_button);
         mSendBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = mNameEditText.getText().toString();
-                Intent intent = new Intent();
-                intent.putExtra("back", name);
-                setResult(RESULT_OK,intent);
-                finish();
+                Intent intent = new Intent(ActivitySecond.this, MainActivity.class);
+                startActivity(intent);
             }
         });
 
+    }
 
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "林宝川测试生命周期onStart: 执行了");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "林宝川测试生命周期onResume: 执行了 ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "林宝川测试生命周期onPause: 执行了");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "林宝川测试生命周期onStop: 执行了");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "林宝川测试生命周期onDestroy: 执行了");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "林宝川测试生命周期onRestart: 执行了");
     }
 }
--- app/src/main/java/com/bc/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/bc/activitylifecycle/MainActivity.java
@@ -13,90 +13,58 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    public static final String BOOK_NAME_OF_CATEGORY_PHYSICAL = "bookNameOfCategoryPhysical";
     private Button mButton;
-    private Button mThirdButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate: 执行了");
+        Log.d(TAG, "林宝川测试生命周期onCreate: 成功执行了");
         mButton = (Button) findViewById(R.id.activity_main_button);
-        final EditText editText = (EditText) findViewById(R.id.activity_main_edit_text);
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Intent intent = new Intent(MainActivity.this,ActivitySecond.class);
-//                startActivity(intent);
-//               Toast.makeText(MainActivity.this, "Button被点击了！", Toast.LENGTH_SHORT).show();
-//                Intent intent  = new Intent(MainActivity.this,ActivitySecond.class);
-//                intent.putExtra(BOOK_NAME_OF_CATEGORY_PHYSICAL,editText.getText().toString());
-//                startActivity(intent);
 
                 Intent intent = new Intent(MainActivity.this, ActivitySecond.class);
-                intent.putExtra(BOOK_NAME_OF_CATEGORY_PHYSICAL,editText.getText().toString());
                 startActivityForResult(intent,0);
             }
         });
 
-        mThirdButton = (Button) findViewById(R.id.activity_main_third_button);
-        mThirdButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, ThirdActivity.class);
-                startActivityForResult(intent,1);
-            }
-        });
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        String returnData = "";
-        switch (requestCode){
-            case 0:
-                returnData = data.getStringExtra("back");
-                break;
-            case 1:
-                returnData = data.getStringExtra("ThirdBack");
-
-        }
-        Toast.makeText(MainActivity.this, returnData, Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart: 执行了");
+        Log.d(TAG, "林宝川测试生命周期onStart: 执行了");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume: 执行了 ");
+        Log.d(TAG, "林宝川测试生命周期onResume: 执行了 ");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "onPause: 执行了");
+        Log.d(TAG, "林宝川测试生命周期onPause: 执行了");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop: 执行了");
+        Log.d(TAG, "林宝川测试生命周期onStop: 执行了");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy: 执行了");
+        Log.d(TAG, "林宝川测试生命周期onDestroy: 执行了");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG, "onRestart: 执行了");
+        Log.d(TAG, "林宝川测试生命周期onRestart: 执行了");
     }
 }
